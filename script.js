abec26fee18f9255b9f7e556071ecef0ea4d6836
Yi-en Tsai <m951235@gmail.com>
2024-07-31 18:42:41 -0700

    Update

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 $(function() {
   // your code here
+  const accordion = $('.accordion')
+  const accordionHeader = $('.accordion-header')
+  const accordionContent = $('.accordion-content')
+  const todosContainer = $('.todos')
+  const list = $('ul')
+
+
+  accordion.find(accordionHeader).on('click',function(){
+    $(this).next(accordionContent).slideToggle()
+  })
+
+  todosContainer.find('button').on('click', async function(){
+    const todoList = await getData()
+    console.log(todoList.todos)
+
+    $.each(todoList.todos, function(index){
+      list.append("<li>list item</li>")
+      $('li').last().text(todoList.todos[index].todo)
+    })
+  })
+
+function getData() {
+    return new Promise((resolve,reject) => {
+      $.ajax({
+        url:'https://dummyjson.com/todos',
+        type: 'GET',
+        success: function(response){
+          resolve(response)
+        },
+        errer: function(error){
+          reject(error)
+        }
+      })
+    })
+  }
+
+
+
 })
